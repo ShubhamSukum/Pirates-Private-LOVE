983bb7d8b64a3a7bda94d4afd64dc49452b0062c
Shubham Sukum <shubhamsukum1722002@gmail.com>
2025-07-30 00:37:37 +0530

    Display fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import React from 'react';
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 function App() {
   const handle = useFullScreenHandle();
-
+                                                                                            
   return (
     <div className="App">
 
@@ -14,7 +13,7 @@ function App() {
 
       <FullScreen handle={handle}>
         <iframe src="https://drive.google.com/file/d/1UmhdNlNSTZbpZ4v-Z6l4gMWL68eGZvtu/preview" 
-        style={{height:"98vh",width:"99vw"}} title='PDF'/>
+        style={{height:"99.47vh",width:"100vw"}} title='Pirate Private LOVE [ bit.ly/shubzPPL1 ]'/>
       </FullScreen>
     </div>
   );
